9d211d709ef9226b196f2ccc57cadabeaab1bad1
wanstar.lin@gmail.com <wanstar.lin@gmail.com>
2025-08-26 19:05:18 +0800

    vue 設定

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,8 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+  publicPath:
+    process.env.NODE_ENV === "production"
+      ? "/minisum-web/" // 這裡請改成你的 repo 名稱
+      : "/",
+});
