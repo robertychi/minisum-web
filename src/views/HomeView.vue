4debe874d089abb0e59f2cd94012a4b867619e87
wanstar.lin@gmail.com <wanstar.lin@gmail.com>
2025-08-28 13:44:54 +0800

    feat(View): 首頁

---
+++ src/views/HomeView.vue
@@ -0,0 +1,25 @@
+<template>
+  <p>Home</p>
+</template>
+<script>
+export default {
+  metaInfo() {
+    return {
+      title: "文章標題 - 我的網站",
+      meta: [
+        { name: "description", content: "這是一篇關於 Vue Meta 的介紹" },
+        { property: "og:title", content: "文章標題 - 我的網站" },
+        { property: "og:image", content: "https://example.com/image.jpg" },
+        { property: "og:description", content: "社群分享的描述內容" },
+      ],
+    };
+  },
+  // component(is='HomeView')
+  name: "HomeView",
+  data() {
+    return {};
+  },
+  computed: {},
+  methods: {},
+};
+</script>
