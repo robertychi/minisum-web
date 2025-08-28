1b105410567adc5f03d76c94efa01a4856d803a0
wanstar.lin@gmail.com <wanstar.lin@gmail.com>
2025-08-28 12:02:46 +0800

    feat(new): 根頁面

---
+++ src/views/RootView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div style="background: red">
+    <router-view></router-view>
+  </div>
+</template>
+<script>
+export default {
+  // component(is='RootView')
+  name: "RootView",
+  data() {
+    return {};
+  },
+  computed: {},
+  methods: {},
+};
+</script>
