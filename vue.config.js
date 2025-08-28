defba50633dad515c8cfa3fa0ec075a127ab08da
wanstar.lin@gmail.com <wanstar.lin@gmail.com>
2025-08-28 18:19:22 +0800

    feat(Js): vue Config

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,25 @@
+const path = require("path");
+
 const { defineConfig } = require("@vue/cli-service");
+
+const VuetifyLoaderPlugin = require("vuetify-loader/lib/plugin");
 module.exports = defineConfig({
   transpileDependencies: true,
   publicPath:
     process.env.NODE_ENV === "production"
       ? "/minisum-web/" // 這裡請改成你的 repo 名稱
       : "/",
+  configureWebpack: {
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "src"),
+        "@com": path.resolve(__dirname, "src/components"),
+        "@views": path.resolve(__dirname, "src/views"),
+        "@plugins": path.resolve(__dirname, "src/plugins"),
+        "@router": path.resolve(__dirname, "src/router"),
+      },
+    },
+    // import Vuetify from 'vuetify/lib/framework' for vuetify.js
+    plugins: [new VuetifyLoaderPlugin()],
+  },
 });
