68f112fbc64025e0613ba9694f14a0e374d755bd
wanstar.lin@gmail.com <wanstar.lin@gmail.com>
2025-08-28 13:44:20 +0800

    feat(js): main.js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import VueMeta from "vue-meta";
 
-Vue.config.productionTip = false
+Vue.use(VueMeta, {
+  refreshOnceOnNavigation: true, // 切換路由時刷新 head 資訊
+});
+
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  router,
+  render: (h) => h(App),
+}).$mount("#app");
