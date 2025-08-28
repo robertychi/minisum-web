c2054e5cb358a46baa739fedf07babef7a8044b4
wanstar.lin@gmail.com <wanstar.lin@gmail.com>
2025-08-29 00:49:42 +0800

    feat(comp): 新增測試組件

---
+++ src/components/TestComponent.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+   p TestComponent
+</template>
+<script>
+import { mapGetters } from "vuex";
+
+export default {
+  // component(is='TestComponent')
+  name: "TestComponent",
+  data() {
+    return {};
+  },
+  computed: {
+    ...mapGetters(["getPage"]),
+  },
+  methods: {},
+};
+</script>
