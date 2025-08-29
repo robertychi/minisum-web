0e38450353c8b08f1790952b16e6c2236dfa1e6e
wanstar.lin@gmail.com <wanstar.lin@gmail.com>
2025-08-29 15:07:50 +0800

    feat(comp): create new component

---
+++ src/components/AppBarNav.vue
@@ -0,0 +1,28 @@
+<template lang="pug">
+  span 
+    v-toolbar-items
+        v-btn(
+          text
+          v-for="item in navItems"
+          :key="item.title"
+          class="mx-2 text-body-1"
+        ) {{ item.title }}
+</template>
+<script>
+export default {
+  // component(is='AppbarNav')
+  name: "AppBarNav",
+  data() {
+    return {
+      navItems: [
+        { title: "核心功能" },
+        { title: "安裝指南" },
+        { title: "常見問題" },
+        { title: "關於我們" },
+      ],
+    };
+  },
+  computed: {},
+  methods: {},
+};
+</script>
